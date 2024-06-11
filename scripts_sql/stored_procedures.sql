ea22fbb87a2d1730b80ec1efb8bfec354f8e0cf2
Julia Meller <juliameller@live.com>
2024-06-11 19:42:34 -0300

    criação stored procedure

---
+++ scripts_sql/stored_procedures.sql
@@ -0,0 +1,30 @@
+-- Registrar Produção Diária
+CREATE PROCEDURE registrar_producao_diaria(@produto_id INT, @data DATE, @quantidade INT) AS 
+BEGIN
+    INSERT INTO Produzido (id_produto, data, quantidade)
+    VALUES (@produto_id, @data, @quantidade);
+END;
+
+CREATE OR ALTER PROCEDURE registrar_producao_diaria(@id_produto INT, @data DATE, @quantidade INT) AS 
+BEGIN
+    DECLARE @producao_existente INT;
+
+    -- Verificar se já existe um registro para esse produto na data especificada
+   	SELECT @producao_existente = quantidade
+    FROM produzido
+    WHERE id_produto = @id_produto AND @data = data
+
+    -- Se existir, atualizar a quantidade
+    IF @producao_existente > 0 
+    	BEGIN
+	        UPDATE produzido
+	        SET quantidade = @producao_existente + @quantidade
+	        WHERE id_produto = @id_produto AND @data = data
+	    END
+    ELSE
+    	BEGIN 
+        INSERT INTO produzido (id_produto, data, quantidade)
+        VALUES (@id_produto, @data, @quantidade)
+    	END
+END
+GO
