3fe1974026b6800537e0bfac23a61d2ea62f144e
Weslaine Santana <161781912+weslainesantana@users.noreply.github.com>
2024-06-17 07:21:04 -0300

    Update indice.sql

--- scripts_sql/indice.sql
+++ scripts_sql/indice.sql
@@ -1,5 +1,9 @@
 -- Criação do índice na tabela pedido_item
-CREATE INDEX idx_pedido_item_id_pedido ON pedido_item(id_pedido);
+CREATE INDEX idx_pedido_item_id_pedido ON pedido_item(id_pedido)
+
+CREATE INDEX idx_pedido_item_id_pedido ON pedido_item(id_pedido) INCLUDE(valor, quantidade);
 
 -- Criação do índice na tabela pagamento
 CREATE INDEX idx_pagamento_id_pedido ON pagamento(id_pedido);
+
+CREATE INDEX idx_pagamento_id_pedido_status_pagamento ON pagamento(id_pedido, status_pagamento) INCLUDE (total);
