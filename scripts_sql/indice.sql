e7e349c39477d561fae4dcdd752f642ad44c16e0
Daniela <89213064+DanielaMF@users.noreply.github.com>
2024-06-18 19:35:21 -0300

    Update indice.sql

--- scripts_sql/indice.sql
+++ scripts_sql/indice.sql
@@ -1,7 +1,7 @@
 -- Criação do índice na tabela pedido_item
 CREATE INDEX idx_pedido_item_id_pedido ON pedido_item(id_pedido)
 
-CREATE INDEX idx_pedido_item_id_pedido ON pedido_item(id_pedido) INCLUDE(valor, quantidade);
+CREATE INDEX idx_pedido_item_id_pedido2 ON pedido_item(id_pedido) INCLUDE(valor, quantidade);
 
 -- Criação do índice na tabela pagamento
 CREATE INDEX idx_pagamento_id_pedido ON pagamento(id_pedido);
